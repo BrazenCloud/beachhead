f60e4c43f88216ecf8de92278a72ab0afbecbed7
Anthony Howell <anthony@howell-it.com>
2022-09-09 14:42:31 -0700

    adding agent deploy script

--- periodicAssignDeploys/windows/script.ps1
+++ periodicAssignDeploys/windows/script.ps1
@@ -23,8 +23,42 @@ $env:BrazenCloudDomain = $settings.host.split('/')[-1]
 $group = (Get-BcAuthenticationCurrentUser).HomeContainerId
 
 # Get agents to deploy
-Invoke-BcQueryDataStore2 -GroupId $group -Query @{query_string = @{query = 'agentInstall'; default_field = 'type' } } -IndexName beachheadconfig
+$agentInstalls = Invoke-BcQueryDataStore2 -GroupId $group -Query @{query_string = @{query = 'agentInstall'; default_field = 'type' } } -IndexName beachheadconfig
 
-# Get all runners
-$runners = 
-Invoke-BcQueryRunner -MembershipCheckId $group -IncludeSubgroups -Skip 0 -Take 1000 -SortDirection 1
+# Get all runners in current group
+$skip = 0
+$take = 1
+$r = Invoke-BcQueryRunner -MembershipCheckId $group -Take $take -Skip $skip -IncludeSubgroups:$false -SortDirection 1
+[BrazenCloudSdk.PowerShell.Models.IRunnerQueryView[]]$runners = $r.Items
+while ($runners.Count -lt $r.FilteredCount) {
+    $skip = $skip + $take
+    Write-Host 'query'
+    Write-Host "Skip: $skip"
+    $r = Invoke-BcQueryRunner -MembershipCheckId $group -Take $take -Skip $skip -IncludeSubgroups:$false -SortDirection 1
+    [BrazenCloudSdk.PowerShell.Models.IRunnerQueryView[]]$runners += $r.Items
+}
+
+# foreach agentInstall, get runners lacking the tag and assign the job
+foreach ($ai in $agentInstalls) {
+    $set = New-BcSet
+    foreach ($runner in $runners | Where-Object { $_.Tags -notcontains $ai.InstalledTag }) {
+        Add-BcSetToSet -TargetSetId $set -ObjectIds $runner.Id
+    }
+    $jobSplat = @{
+        Name          = "Beachhead Deploy $($ai.Name)"
+        GroupId       = $group
+        EndpointSetId = $set
+        IsEnabled     = $true
+        IsHidden      = $false
+        Actions       = foreach ($action in $ai.actions) {
+            @(
+                @{
+                    RepositoryActionId = (Get-BcRepository -Name $action.action).Id
+                    Settings           = $actions.settings
+                }
+            )
+        }
+        Schedule      = New-RwJobScheduleObject -ScheduleType 'RunNow'
+    }
+    New-BcJob @jobSplat
+}
