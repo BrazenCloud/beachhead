678b245eac2404bd7adc3da81f15ec476b0871e8
Anthony Howell <anthony@howell-it.com>
2022-09-22 14:31:41 -0700

    adding verbosity, fixing cleanup

--- alternateDeploys/windows/script.ps1
+++ alternateDeploys/windows/script.ps1
@@ -28,33 +28,46 @@ Get-BcAgentExecutable -Platform Windows64 -OutFile .\runner.exe
 if ($settings.'PowerShell Remoting'.ToString() -eq 'true') {
     #region RemotePowerShell
     # get the host name
-    $name = (Resolve-DnsName 192.168.77.6).NameHost
+    $name = (Resolve-DnsName $settings.'IP Range').NameHost
 
     # create the session
     $session = New-PSSession $name
 
     # Copy runner.exe and runway.exe to remote host
-    Copy-Item .\runner.exe -Destination C:\temp\runner.exe -ToSession $session
-    Copy-Item ..\..\runway.exe -Destination C:\temp\runway.exe -ToSession $session
+    Copy-Item ..\..\..\runner.exe -Destination C:\runner.exe -ToSession $session
+    Copy-Item ..\..\..\runway.exe -Destination C:\runway.exe -ToSession $session
 
     # Execute the script
-    $str1 = (Get-Content .\repoScripts\functions\Enrollment.ps1 -Raw)
+    $str1 = (Get-Content .\windows\dependencies\Enrollment.ps1 -Raw)
     $sb2 = {
-        $execPath = 'C:\temp\runner.exe'
-        $utilityPath = 'C:\temp\runway.exe'
+        $execPath = 'C:\runner.exe'
+        $utilityPath = 'C:\runway.exe'
         $token = $using:settings.'Enrollment Token'
-        $agentDetails = Get-BcAgentDetails -UtilityPath $utilityPath -EnrollmentToken $token
+        Write-Host "Retrieving agent details..."
+        $agentDetails = Get-BcAgentDetails -UtilityPath $utilityPath
+        Write-Host "Requesting enrollment..."
         $enrollment = Get-BcAgentEnrollment -EnrollmentToken $token -Parameters $agentDetails
+        Write-Host "Installing agent..."
         Install-BcAgent -EnrollResponse $enrollment -AgentExecutablePath $execPath -EnrollmentToken $token
+        Write-Host "Cleaning..."
+        if (Test-Path C:\runner.exe) {
+            Remove-Item C:\runner.exe -Force
+        }
+        if (Test-Path C:\runway.exe) {
+            Remove-Item C:\runway.exe -Force
+        }
+        Write-Host "Complete"
     }
-    Invoke-Command -Session $session -ScriptBlock [scriptblock]::Create(($str1 + $sb2.ToString()))
+    Invoke-Command -Session $session -ScriptBlock ([scriptblock]::Create(($str1 + $sb2.ToString())))
+
+    Remove-PSSession $session
 
     #endregion
 }
 
 if ($settings.WMI.ToString() -eq 'true') {
     #region WMI
-    $name = (Get-WmiObject -Class Win32_ComputerSystem -ComputerName 192.168.77.6).Name
+    $name = (Get-WmiObject -Class Win32_ComputerSystem -ComputerName $settings.'IP Range').Name
 
     $sb = {
         $execPath = 'C:\runner.exe'
--- repoScripts/functions/Enrollment.ps1
+++ repoScripts/functions/Enrollment.ps1
@@ -27,13 +27,11 @@ Function Get-BcAgentDetails {
     param (
         [Parameter(Mandatory)]
         [string]$UtilityPath,
-        [string]$Server = 'staging.brazencloud.com',
-        [Parameter(Mandatory)]
-        [string]$EnrollmentToken
+        [string]$Server = 'staging.brazencloud.com'
     )
     $regex = '\> Enrollment: (Using ?)?(?<name>[^ ]+) (?<value>.*)\.'
     #        > Enrollment: UsingDesiredRunnerName default.
-    $enrollment = & $UtilityPath -N -S $Server node -t $EnrollmentToken -d 0.00:00:00 --new | Where-Object { $_ -like '> Enrollment:*' }
+    $enrollment = & $UtilityPath -N -S $Server node -t 'blah' -d 0.00:00:00 --new | Where-Object { $_ -like '> Enrollment:*' }
     $ht = @{}
     foreach ($line in $enrollment) {
         if ($line -match $regex) {
@@ -66,6 +64,60 @@ Function Get-BcAgentEnrollment {
     }
 }
 
+Function Set-BcAgentPermissions {
+    [cmdletbinding()]
+    param (
+        [string]$AgentExecutablePath
+    )
+    # Allow full access to administrators
+    # deny access to guests
+    # deny access to ANONYMOUS LOGON
+    # Owner = comp\administrators
+    # Nothing else
+    if (-not (Test-Path $AgentExecutablePath -PathType Leaf)) {
+        throw "'$AgentExecutablePath' is not a valid path."
+    }
+    $acl = [System.Security.AccessControl.FileSecurity]::new()
+    #$acl = Get-Acl $AgentExecutablePath
+    # clear acl
+    #foreach ($ace in $acl.Access) {
+    #    $acl.RemoveAccessRuleAll($ace)
+    #}
+    # full control to admins
+    $adminAce = [System.Security.AccessControl.FileSystemAccessRule]::new(
+        [System.Security.Principal.NTAccount]'Builtin\Administrators',
+        [System.Security.AccessControl.FileSystemRights]::FullControl,
+        [System.Security.AccessControl.InheritanceFlags]::None,
+        [System.Security.AccessControl.PropagationFlags]::None,
+        [System.Security.AccessControl.AccessControlType]::Allow
+    )
+    # deny guests
+    $guestAce = [System.Security.AccessControl.FileSystemAccessRule]::new(
+        [System.Security.Principal.NTAccount]'Builtin\Guests',
+        [System.Security.AccessControl.FileSystemRights]::FullControl,
+        [System.Security.AccessControl.InheritanceFlags]::None,
+        [System.Security.AccessControl.PropagationFlags]::None,
+        [System.Security.AccessControl.AccessControlType]::Deny
+    )
+    # deny anonymous logon
+    $alAce = [System.Security.AccessControl.FileSystemAccessRule]::new(
+        [System.Security.Principal.NTAccount]'ANONYMOUS LOGON',
+        [System.Security.AccessControl.FileSystemRights]::FullControl,
+        [System.Security.AccessControl.InheritanceFlags]::None,
+        [System.Security.AccessControl.PropagationFlags]::None,
+        [System.Security.AccessControl.AccessControlType]::Deny
+    )
+    $acl.AddAccessRule($adminAce)
+    $acl.AddAccessRule($guestAce)
+    $acl.AddAccessRule($alAce)
+    $acl.SetOwner([System.Security.Principal.NTAccount]'Builtin\Administrators')
+    $acl.SetAccessRuleProtection($true, $true)
+    foreach ($item in (Get-ChildItem (Get-Item $AgentExecutablePath).Directory.FullName -File)) {
+        Write-Host "Applying ACL to $($item.Name)"
+        Set-Acl $item.FullName -AclObject $acl
+    }
+}
+
 Function Install-BcAgent {
     [cmdletbinding()]
     param (
@@ -85,7 +137,8 @@ Function Install-BcAgent {
         [string]$AgentExecutablePath,
         [Parameter(Mandatory)]
         [string]$EnrollmentToken,
-        [string]$Server = 'staging.brazencloud.com'
+        [string]$Server = 'staging.brazencloud.com',
+        [switch]$MoveAgentExecutable
     )
     $pfiles = $env:ProgramFiles
     if (-not (Test-Path $pfiles\Runway)) {
@@ -97,7 +150,11 @@ Function Install-BcAgent {
     if (-not (Test-Path $fullInstallPath)) {
         New-Item $fullInstallPath -ItemType Directory | Out-Null
     }
-    Copy-Item $AgentExecutablePath -Destination $fullInstallPath\runner.exe
+    if ($MoveAgentExecutable.IsPresent) {
+        Move-Item $AgentExecutablePath -Destination $fullInstallPath\runner.exe
+    } else {
+        Copy-Item $AgentExecutablePath -Destination $fullInstallPath\runner.exe
+    }
 
     $x = 0
     $serviceName = "RunwayRunnerService"
@@ -120,6 +177,7 @@ Function Install-BcAgent {
     [System.IO.File]::WriteAllLines("$fullInstallPath\runner_settings.json", $json, [System.Text.UTF8Encoding]::new($false))
 
     $out = & sc.exe create $serviceName type=own start=auto error=normal binPath="\`"$fullInstallPath\runner.exe\`" service" displayname=$displayName
+    Set-BcAgentPermissions -AgentExecutablePath $fullInstallPath\runner.exe
     if ($out -like '*SUCCESS') {
         Start-Service $serviceName
     } else {
