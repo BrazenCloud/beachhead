7a26e0884f1552264f5b4a57bce62329d154e2f3
Anthony Howell <anthony@howell-it.com>
2022-10-06 13:41:13 -0700

    adding demo prep script

---
+++ repoScripts/demo/prepDemo.ps1
@@ -0,0 +1,25 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$GroupName
+)
+
+$agentInstalls = Get-Content $PSScriptRoot\agentInstalls.json | ConvertFrom-Json
+
+if (-not (Get-BcAuthenticationCurrentUser)) {
+    Throw 'Connect to BrazenCloud using Connect-BrazenCloud.'
+}
+
+$group = (Get-BcGroup).Items | Where-Object { $_.Name -eq $GroupName }
+
+if (-not ($null -ne $group -and $group.Count -eq 1)) {
+    Throw 'Group Name does not match any group.'
+}
+
+. $PSScriptRoot\..\functions\Invoke-BcBulkDatastoreInsert2.ps1
+. $PSScriptRoot\..\functions\Remove-BcDatastoreQuery2.ps1
+
+try {
+    Remove-BcDatastoreQuery2 -IndexName 'beachheadconfig' -Query @{query = @{match_all = @{} } }
+} catch {}
+
+Invoke-BcBulkDatastoreInsert2 -IndexName 'beachheadconfig' -GroupId $group.Id -Data $agentInstalls
