c112d2ec1cb04dbbccf0cb5ad9d4c0b8cacf17f5
Anthony Howell <anthony@howell-it.com>
2022-10-18 15:07:24 -0700

    removing some output

--- repoScripts/updateDependencies.ps1
+++ repoScripts/updateDependencies.ps1
@@ -24,7 +24,6 @@ foreach ($script in $sc.Keys) {
 # update script dependencies third
 $regex = '^\#region dependencies(\r\n|\n)(\. \.\\.*(\r\n|\n))*\#endregion'
 foreach ($dir in (Get-ChildItem $PSScriptRoot\..\ -Directory)) {
-    Write-Host $dir.Name
     $scriptPath = "$($Dir.FullName)\windows\script.ps1"
     if (Test-Path $scriptPath) {
         $content = Get-Content $scriptPath -Raw
@@ -33,11 +32,10 @@ foreach ($dir in (Get-ChildItem $PSScriptRoot\..\ -Directory)) {
             $str = foreach ($d in $depends) {
                 ". .\windows\dependencies\$d"
             }
-            "#region dependencies`n$($str -join "`n")`n#endregion"
             $content = $content -replace $regex, "#region dependencies`n$($str -join "`n")`n#endregion"
             $content.Trim() | Out-File $scriptPath -Encoding utf8 -NoNewline
         } else {
-            Write-Host 'not match regex'
+
         }
     }
 }
