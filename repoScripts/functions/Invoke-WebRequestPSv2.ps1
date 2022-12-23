5ac251579f57ec6df0a4fc912f7e1cf436b3bf5c
Anthony Howell <anthony@howell-it.com>
2022-12-23 10:09:24 -0800

    adding body

--- repoScripts/functions/Invoke-WebRequestPSv2.ps1
+++ repoScripts/functions/Invoke-WebRequestPSv2.ps1
@@ -3,19 +3,37 @@ Function Invoke-WebRequestPSv2 {
         [string]$URI,
         [string]$Method = 'Get',
         [hashtable]$Headers,
-        [switch]$RawResponse
+        [switch]$RawResponse,
+        [string]$Body
     )
 
     #tls1.2
     $p = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072)
     [System.Net.ServicePointManager]::SecurityProtocol = $p
 
+    # Create the webrequest
     $WebRequest = [System.Net.WebRequest]::Create($URI)
+    # Set Method and content type
     $WebRequest.Method = $Method
+    $WebRequest.ContentType = "application/json"
+    # Add headers
     foreach ($h in $Headers.Keys) {
         $WebRequest.Headers.Add($h, $Headers[$h])
     }
-    $WebRequest.ContentType = "application/json"
+
+    # add body, if exists
+    if ($Body.Length -gt 0) {
+        $bodyBytes = [System.Text.Encoding]::UTF8.GetBytes($Body)
+
+        $WebRequest.ContentLength = $bodyBytes.Length
+
+        $rstream = $WebRequest.GetRequestStream()
+        $rstream.Write($bodyBytes, 0, $bodyBytes.Length)
+        $rstream.Close()
+    } else {
+        $WebRequest.ContentLength = 0
+    }
+
     $Response = $WebRequest.GetResponse()
     if ($RawResponse.IsPresent) {
         $Response
