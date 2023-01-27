0c38538930adb117af121af3f728d967e3d63d9f
Anthony Howell <anthony@brazencloud.com>
2023-01-27 11:44:16 -0800

    updating tee-bclog

--- repoScripts/functions/Tee-BcLog.ps1
+++ repoScripts/functions/Tee-BcLog.ps1
@@ -1,23 +1,19 @@
 Function Tee-BcLog {
     [cmdletbinding()]
     param (
-        [Parameter(Mandatory)]
         [ValidateSet('Info', 'Error')]
         [string]$Level,
-        [Parameter(Mandatory)]
         [string]$Message,
-        [Parameter(Mandatory)]
         [string]$JobName,
-        [Parameter(Mandatory)]
         [string]$Group
     )
     $timestamp = (Get-Date -Format 'o')
     $logHt = [ordered]@{
-        TimeStamp = $timestamp
-        Level     = $Level
-        Job       = $JobName
-        Message   = $Message
+        timeStamp = $timestamp
+        level     = $Level
+        job       = $JobName
+        message   = $Message
     }
-    Invoke-BcBulkDataStoreInsert -GroupId $Group -IndexName 'beachheadlogs' -Data ($logHt | ForEach-Object { ConvertTo-Json $_ -Compress })
+    Invoke-BcBulkDataStoreInsert -GroupId $Group -IndexName 'beachheadlogs' -Data ($logHt | ForEach-Object { ConvertTo-Json $_ -Compress }) | Out-Null
     Write-Host "$timestamp [$Level] $Message"
 }
